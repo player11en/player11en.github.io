7737ff8c191b959e869326eb1726e509a0b0f318
playereleven <Zina.Ghannadan@stud.hshl.de>
2020-09-09 12:32:18 +0200

    hii

--- Three.js/js/main.js
+++ Three.js/js/main.js
@@ -234,7 +234,7 @@
                 // create a global audio source
                 sound = new THREE.Audio(listener);
     
-                var material = new THREE.MeshBasicMaterial({ map : videoTexture, transparent : true, side: THREE.DoubleSide });
+                var material = new THREE.MeshBasicMaterial({ map : videoTexture, transparent : true });
                 //plane = new THREE.Mesh(new THREE.PlaneGeometry(4, 2));
                 plane = new THREE.Mesh(new THREE.PlaneGeometry(4, 2), material);
                 plane.position.set(0,1,0);
