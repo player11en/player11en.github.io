9fa4a66c0b24872ae58f1c1660ede6648cf21293
Zina Ghannadan <zina_scorpion@hotmail.com>
2023-02-03 05:23:17 +0100

    gh

--- Babylonjs/Sofa/sw.js
+++ Babylonjs/Sofa/sw.js
@@ -1,56 +1,53 @@
-var APP_PREFIX = 'Sofa'    
-var VERSION = 'version_03'      
-var CACHE_NAME = APP_PREFIX + VERSION        
-var URLS = [    
-    '/index.html',
-    '/sw.js',
-    '/images/favicon.ico',
-    '/models/Sofa.glb'          
+const GHPATH = 'https://player11en.github.io/Babylonjs/Sofa';
+var APP_PREFIX = 'Sofa'
+var VERSION = 'version_03'
+const CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+    `${GHPATH}/`,
+    `${GHPATH}/index.html`,
+    `${GHPATH}/sw.js`,
+    `${GHPATH}/images/favicon.ico`,
+    `${GHPATH}/models/Sofa.glb`,
 ]
+
+
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+})
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME);
-            return cache.addAll(URLS);
+            console.log('Installing cache : ' + CACHE_NAME);
+            return cache.addAll(URLS)
         })
-    );
-});
+    )
+})
 
-// Delete outdated caches
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-      caches.keys().then(function (keyList) {
-        // `keyList` contains all cache names under your username.github.io
-        // filter out ones that has this app prefix to create white list
-        var cacheWhitelist = keyList.filter(function (key) {
-          return key.indexOf(APP_PREFIX)
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            cacheWhitelist.push(CACHE_NAME);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i])
+                }
+            }))
         })
-        // add current cache name to white list
-        cacheWhitelist.push(CACHE_NAME)
-  
-        return Promise.all(keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i] )
-            return caches.delete(keyList[i])
-          }
-        }))
-      })
     )
-  })
-
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-      })
-    )
-  })
+})
