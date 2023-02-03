3a9ce3699c94342debcc029abc74b65af9db61ee
Zina Ghannadan <zina_scorpion@hotmail.com>
2023-02-03 02:25:46 +0100

    jz

--- Babylonjs/Sofa/sw.js
+++ Babylonjs/Sofa/sw.js
@@ -1,16 +1,3 @@
-// // Register a service worker
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function () {
-//         navigator.serviceWorker.register('/sw.js').then(function (registration) {
-//             // Registration was successful
-//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//         }, function (err) {
-//             // Registration failed
-//             console.log('ServiceWorker registration failed: ', err);
-//         });
-//     });
-// }
-
 // Install event
 self.addEventListener('install', function (event) {
     console.log('ServiceWorker installed');
