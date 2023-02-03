fbbfc63254b3a82d6f2ae09e027bf7c382e7e134
Zina Ghannadan <zina_scorpion@hotmail.com>
2023-02-03 04:16:58 +0100

    rec

--- Babylonjs/Sofa/sw.js
+++ Babylonjs/Sofa/sw.js
@@ -4,7 +4,7 @@ var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [               // Add URL you want to cache in this list.
   '/sw.js',
   '/images/favicon.ico',
-  '/models/Sofa.glb'            // add path to those files here
+  '/models/Sofa.glb',            // add path to those files here
 ]
 // Install event
 self.addEventListener('install', function (e) {
@@ -42,12 +42,12 @@ self.addEventListener('activate', function (e) {
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
+      caches.match(e.request).then(function (respond) {
+        if (respond) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.respond.url)
+          return respond
         } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
+          console.log('file is not cached, fetching : ' + e.respond.url)
           return fetch(e.request)
         }
       })
